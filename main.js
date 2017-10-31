ef39635edbb87379a7caa64a9e16652f93896d73
Evgeniya <jenyapawko@gmail.com>
2017-10-31 23:48:50 +0200

    digital keyboard fixed

--- main.js
+++ main.js
@@ -1,49 +1,52 @@
 $(document).ready(function() {
-	$(".btn").click(function(e) {
-		var btn = $(this),
-			entryField = $("#entry_field");
+    $(".btn").click(function(e) {
+        var btn = $(this),
+            entryField = $("#entry_field");
 
-		// check the number of characters to be entered in input by mouse click
-        	if(entryField.val().length > 3) {
-            	return entryField.val();           
-       		};
+        // check the number of characters to be entered in input by mouse click
+        if (entryField.val().length > 3) {
+            return entryField.val();
+        };
 
+
+        // check the number of characters to be entered in input by keyboard
+        $("#entry_field").keyup(function() {
+
+            if (entryField.val().length > 4) {
+                return entryField.val();
+            };
+        });
+
+        entryField.val(entryField.val() + btn.val());
+    });
+});
+
+$(".btn").click(function() {
+    var originalBtn = $("#entry_field"),
+        newBtn = originalBtn.clone();
         
-		// check the number of characters to be entered in input by keyboard
-		$("#entry_field").keyup(function() {
-        	
-        	if(entryField.val().length > 4) {
-        		return entryField.val();           
-       		};
-    	});
-        	
-	 	entryField.val(entryField.val() + btn.val());    
-	});
+    newBtn.insertBefore(originalBtn);
+    originalBtn.remove();
+    newBtn.attr('type', 'password');
+
 });
 
 
-	// cancel the last entering number
-	$(".btnControl01").click(function() {
-		var entryField = $("#entry_field").val(),
-			cancelNum = entryField.substr(0, (entryField.length - 1));
-		$("#entry_field").val(cancelNum);
-	}); 
+// cancel the last entering number
+$(".btnControl01").click(function() {
+    var entryField = $("#entry_field").val(),
+        cancelNum = entryField.substr(0, (entryField.length - 1));
+    $("#entry_field").val(cancelNum);
+});
 
 
-	// remove the entering numbers from the screen
-	$(".btnControl02").click(function() {
-		$("#entry_field").val("");
-	});
-
-	
-	// show the entering result in alert window
-	$(".btnControl03").click(function() {
-		alert($("#entry_field").val());
-	});
+// remove the entering numbers from the screen
+$(".btnControl02").click(function() {
+    $("#entry_field").val("");
+});
 
 
-
-
-
-	
-	
+// show the entering result in alert window
+$(".btnControl03").click(function() {
+    alert($("#entry_field").val());
+});
